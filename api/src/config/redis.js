a89a8c6488f0fe2fbcb6e49a3e92535a5548f5ef
Jens van Hellemondt <jens.vanhellemondt@gmail.com>
2021-01-17 08:36:38 +0100

    :recycle: redis setup

--- api/src/config/redis.js
+++ api/src/config/redis.js
@@ -1,14 +1,11 @@
-import Redis from 'ioredis';
+import redis from 'redis';
 import { REDIS_HOST, REDIS_PORT } from './constants';
 
 export default new Promise((resolve, reject) => {
-  const redisConnectionString = `${REDIS_HOST}:${REDIS_PORT}`;
+  const redisConnectionString = `redis://${REDIS_HOST}:${REDIS_PORT}`;
   console.warn(`🌐 Connecting to Redis on ${redisConnectionString}`);
 
-  const redisClient = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-  });
+  const redisClient = redis.createClient({ url: redisConnectionString });
   redisClient.on('error', (error) => reject(`Redis connection error: ${error}`));
   redisClient.once('open', () => {
     console.info('Redis connected');
